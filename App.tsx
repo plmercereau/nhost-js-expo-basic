20051d84bdc6f7488be52159e7a6bb2113ec9ae7
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-04-21 16:10:03 +0200

    chore: enable storage

--- App.tsx
+++ App.tsx
@@ -1,19 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
-import { NhostClient } from '@nhost/nhost-js';
+import { NhostClient } from '@nhost/nhost-js'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const nhost = new NhostClient({
-  backendUrl: 'https://xxx.nhost.run'
+  backendUrl: 'https://gfambeospnuqjpvlvtku.nhost.run',
+  clientStorageType: 'react-native',
+  clientStorage: AsyncStorage,
+  refreshIntervalTime: 10,
+})
+
+nhost.auth.onTokenChanged(e => {
+  console.log('New access token', e?.accessToken)
 })
 
 export default function App() {
+  console.log('App component')
+
+  const signUp = async () => {
+    console.log('SIGNUP')
+    const result = await nhost.auth.signUp({
+      email: 'pilou@pilou.com',
+      password: 'Pilette1',
+    })
+    console.log(result)
+  }
+  const signIn = async () => {
+    console.log('SIGN IN')
+    const result = await nhost.auth.signIn({
+      email: 'pilou@pilou.com',
+      password: 'Pilette1',
+    })
+    console.log(result)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Voila</Text>
+      <Button onPress={signUp} title='Sign up'>
+        Sign Up!
+      </Button>
+      <Button onPress={signIn} title='Sign In'>
+        Sign In!
+      </Button>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -23,4 +56,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
